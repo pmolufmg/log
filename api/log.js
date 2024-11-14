bc9fb05017847e0cbea98327c240dbdb96734ae8
Paulo Mol <paulo.junior@robbyson.com>
2024-11-14 18:53:40 -0300

    feat: log to console

--- api/log.js
+++ api/log.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
     if (req.method === 'POST') {
         const { timestamp, videoId, ip } = req.body;
         console.log(`Access: ${timestamp} - Vídeo: ${videoId} - IP: ${ip}`);
+        console.info(`Access: ${timestamp} - Vídeo: ${videoId} - IP: ${ip}`);
         res.status(200).send({ message: 'Ok' });
     } else {
         res.status(405).send({ message: 'Error' });
